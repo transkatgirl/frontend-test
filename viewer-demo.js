0258ab47a8049240b4970f919cabe014906bb21a
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-06-28 04:12:33 -0700

    Bug fixes

--- viewer-demo.js
+++ viewer-demo.js
@@ -1,3 +1,3 @@
-let textbook = new Textbook("epub_unpacked", "./textbook-scraper/test.epub", {});
+let textbook = new Textbook("epub_unpacked", "./textbook-scraper/test.epub", { sandbox: false });
 
 textbook.then((textbook) => textbook.render());
