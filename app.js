6edec588ac6abc318ba2e327e081d02e3f3542ee
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-01 09:59:41 -0700

    Added completion calculation

--- app.js
+++ app.js
@@ -153,6 +153,53 @@ class CourseBook {
 			}
 		}
 	}
+	get completion() {
+		let completion = [];
+
+		for (const chapter of this.chapters) {
+			if (this.completed.has(chapter.id)) {
+				completion.push(1);
+			} else if (chapter.sections) {
+				let chapterCompletionValue = 0;
+				let chapterCompletionItems = 0;
+
+				for (let section of chapter.sections) {
+					if (typeof section == "string") {
+						if (this.completed.has(section)) {
+							chapterCompletionValue += 1;
+						}
+					} else {
+						let sectionGroup = section;
+						let sectionGroupCompletionValue = 0;
+						let sectionGroupCompletionItems = 0;
+
+						for (let section of sectionGroup) {
+							if (this.completed.has(section)) {
+								sectionGroupCompletionValue += 1;
+							}
+							sectionGroupCompletionItems += 1;
+						}
+
+						if (sectionGroupCompletionItems > 0) {
+							chapterCompletionValue += sectionGroupCompletionValue / sectionGroupCompletionItems;
+						}
+					}
+					chapterCompletionItems += 1;
+				}
+
+				if (chapterCompletionItems > 0) {
+					completion.push(chapterCompletionValue / chapterCompletionItems);
+				} else {
+					completion.push(0);
+				}
+
+			} else {
+				completion.push(0);
+			}
+		}
+
+		return completion;
+	}
 	load({ cssUrl }) {
 		if (!this.#textbook) {
 			this.prefetch();
