6bebe524e4da410b15df9b5c005081023feb5deb
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-03 09:28:02 -0700

    Started work on implementing BookChapterGraph

--- scripts/data-graphing.js
+++ scripts/data-graphing.js
@@ -1,13 +1,9 @@
 class TimeProgressMeter {
 	#dataElements;
-	constructor (size, min, max) {
-		if (size && size > 0) {
-			this.size = Number(size);
-		} else {
-			this.size = 5;
-		}
+	constructor (min = 0, max = 150, size = 5) {
 		this.min = Number(min);
 		this.max = Number(max);
+		this.size = Number(size);
 	}
 	render() {
 		if (!this.element) {
@@ -57,7 +53,51 @@ class TimeProgressMeter {
 }
 
 class BookChapterGraph {
+	#dataElements;
+	constructor (title, chapters, width = 10) {
+		if (title) {
+			this.title = String(title);
+		}
+		this.chapters = Number(chapters);
+		this.width = Number(width);
+	}
+	render() {
+		if (!this.element) {
+			this.element = document.createElement("table");
+			this.element.setAttribute("class", "progress-map chapter-progress-map");
+			if (this.title) {
+				const caption = document.createElement("caption");
+				caption.innerText = "📖 " + this.title;
+				this.element.appendChild(caption);
+			}
 
+			const body = document.createElement("tbody");
+
+			this.#dataElements = [];
+
+			let height = Math.ceil(this.chapters / this.width);
+
+			for (let ii = 0; ii < height; ii++) {
+				const row = document.createElement("tr");
+				for (let i = 0; i < this.width; i++) {
+					const dataElement = document.createElement("td");
+					this.#dataElements.push(dataElement);
+					row.appendChild(dataElement);
+					if (this.#dataElements.length >= this.chapters) {
+						break;
+					}
+				}
+				body.appendChild(row);
+			}
+
+			this.element.appendChild(body);
+		}
+
+		return this.element;
+	}
+	update(progress) {
+
+	}
 }
 
 class DailyProgressGraph {
